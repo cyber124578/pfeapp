824c2e1101c2f38784676b449d2011ccd5dab82a
firas bentaleb <firasbt20182@gmail.com>
2020-08-25 13:32:11 -0700

    app ready

---
+++ app/src/main/java/com/example/pfe/Activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.pfe.Activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pfe.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
